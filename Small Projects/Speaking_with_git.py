55d26188043034d65e0fb307134ebde87dce0066
Matt W <matthew.watwood@colorado.edu>
2021-09-24 10:39:55 -0600

    Reorganizing

---
+++ Small Projects/Speaking_with_git.py
@@ -0,0 +1,21 @@
+### This is a Test file to show the usefulness of git and working in teams
+
+### This is Matt's section to give a demonstration.
+print("Backcountry skiing is just hiking without the people and more chances to fall into white quicksand.")
+print("")
+print("Haikus are easy")
+print("But they don't always make sense")
+print("Refrigerator")
+
+### Here is space for Mikell to make a statement about his dogs or his favorite pastry
+print("Mikell says:")
+
+### Here Sean can tell us about his favorite place in the world
+print("Then Sean comes along and says:")
+
+### Here Nathan can tell us about his favorite food
+print("Then Nathan enters the converstation with:")
+
+### Here Mckenzie can tell us about her cats, dog, or favorite Disney movie/show
+print("Finally Mckenzie jumps in to add")
+
