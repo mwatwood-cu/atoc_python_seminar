311d30aba53afe2cae687ef0b82092038cd28bcd
Matt W <matthew.watwood@colorado.edu>
2021-09-24 10:31:33 -0600

    Adding a better way to commit from different git profiles

---
+++ Weekly Notes/Speaking_with_git.py
@@ -0,0 +1,21 @@
+### This is a Test file to show the usefulness of git and working in teams
+
+### This is Matt's section to give a demonstration.
+print("Backcountry skiing is just hiking without the people and more chances to fall into white quicksand.")
+print("")
+print("Haikus are easy")
+print("But they don't always make sense")
+print("Refrigerator")
+
+### Here is space for Mikell to make a statement about his dogs or his favorite pastry
+
+
+### Here Sean can tell us about his favorite place in the world
+
+
+### Here Nathan can tell us about his favorite food
+
+
+### Here Mckenzie can tell us about her cats, dog, or favorite Disney movie/show
+
+
