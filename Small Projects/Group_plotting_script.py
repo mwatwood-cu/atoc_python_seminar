5448dbf302cf1b2b988cb5b2192cd2ac7bf0f2f8
sleister <90477408+sleister@users.noreply.github.com>
2021-10-15 13:20:12 -0600

    mckenzie is awesome ^2

--- Small Projects/Group_plotting_script.py
+++ Small Projects/Group_plotting_script.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from Plotting_tools import example
-#from Plotting_tools import create_monthly_means
-#from Plotting_tools import plot_means_surrounded_by_deviation
+# from Plotting_tools import create_monthly_means
+from Plotting_tools import plot_means_surrounded_by_deviation
 
 # Use this file for testing!
 
@@ -14,14 +14,14 @@ example()
 print("Finishing the code")
 
 
-
 # Group one testing
-file_name = "../Data/CERES20YearAvgGlobalCloudCover.nc"
-global_cloud_time_series = xr.open_dataset(file_name)
+# file_name = "../Data/CERES20YearAvgGlobalCloudCover.nc"
+# global_cloud_time_series = xr.open_dataset(file_name)
 
-dataFrame = create_monthly_means(global_cloud_time_series)
+# dataFrame = create_monthly_means(global_cloud_time_series)
 
-# Group two testing 
-file_name_2 = "../Data/monthlyTestData.nc"
-fill_in_data = xr.open_dataset(file_name_2 )
-plot_means_surrounded_by_deviation(fill_in_data)
+# Group two testing
+file_name_2 = "Data/monthlyTestData.nc"
+fill_in_data = xr.open_dataset(file_name_2)
+data = fill_in_data.to_dataframe()
+plot_means_surrounded_by_deviation(data)
