2bb8dd5d50bdbf30b992d448b579f316f6c0acc5
jj-mauss <jjmauss1234@gmail.com>
2021-10-15 14:02:04 -0600

    maybe this will work now

--- Small Projects/Group_plotting_script.py
+++ Small Projects/Group_plotting_script.py
@@ -1,27 +1,30 @@
 import xarray as xr
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-from Plotting_tools import example
-#from Plotting_tools import create_monthly_means
+#from Plotting_tools import example
+from Plotting_tools import create_monthly_means
 #from Plotting_tools import plot_means_surrounded_by_deviation
 
 # Use this file for testing!
 
 # First an example to show debugging and running
 print("Starting the code")
-example()
+#example()
 print("Finishing the code")
 
 
 
 # Group one testing
-file_name = "../Data/CERES20YearAvgGlobalCloudCover.nc"
+file_name = "./Data/CERES20YearAvgGlobalCloudCover.nc"
 global_cloud_time_series = xr.open_dataset(file_name)
 
 dataFrame = create_monthly_means(global_cloud_time_series)
 
+
+'''
 # Group two testing 
 file_name_2 = "../Data/monthlyTestData.nc"
 fill_in_data = xr.open_dataset(file_name_2 )
 plot_means_surrounded_by_deviation(fill_in_data)
+'''
